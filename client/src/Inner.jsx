90dc0ad136186dea2896cc561c90bf7dc9b24523
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-06 23:26:40 -0500

    [WIP] Added fake static image and details

--- client/src/Inner.jsx
+++ client/src/Inner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {
-  Container,
+  Header,
   Image,
   Grid,
   Segment,
@@ -14,6 +14,23 @@ const Inner = () => (
       <Grid container columns={2} verticalAlign="middle">
         <Grid.Column>
           <Image src="https://source.unsplash.com/random/600x337" />
+          <Header as="h3" content="Details" />
+          <Segment basic compact vertical>
+            The stranger officiates the meal.
+          </Segment>
+          <Segment basic compact vertical>
+            Sometimes, all you need to do is completely make a fool of yourself
+              and laugh it off to realize that life is not so bad after all.
+          </Segment>
+          <Segment basic compact vertical>
+            You don't need to have a flame in order to get married.
+          </Segment>
+          <Segment basic compact vertical>
+            Quit milk.
+          </Segment>
+          <Segment basic compact vertical>
+            If you have any questions feel free to reach out to our team at <a href="http://inspirobot.me">insiprobot.me</a>
+          </Segment>
           {/* Other components */}
         </Grid.Column>
         <Grid.Column>
